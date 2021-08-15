5395a3f5c5eb768ab2fdcd1144b4093b42d83630
ALezhnin <lezhnin.alex@gmail.com>
2021-08-15 13:39:02 +0300

    #00504 Gendiff: change os.sep path to fixtures

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -3,6 +3,8 @@ import pytest
 from gendiff.diff_engine import generate_diff
 from shutil import copy2
 import sys
+from os import sep
+FIXTURE_PATH = sys.path[0] + sep + 'fixtures' + sep
 
 
 def copy_files(tmpdir_factory, file1, file2, extension):
@@ -20,8 +22,8 @@ def copy_json_files(tmpdir_factory):
     """Copy json files."""
     return copy_files(
         tmpdir_factory,
-        sys.path[0] + r'\fixtures\1_1.json',
-        sys.path[0] + r'\fixtures\1_2.json',
+        FIXTURE_PATH + '1_1.json',
+        FIXTURE_PATH + '1_2.json',
         'json'
     )
 
@@ -31,8 +33,8 @@ def copy_yaml_files(tmpdir_factory):
     """Copy yaml files."""
     return copy_files(
         tmpdir_factory,
-        sys.path[0] + r'\fixtures\2_1.yaml',
-        sys.path[0] + r'\fixtures\2_2.yaml',
+        FIXTURE_PATH + '2_1.yaml',
+        FIXTURE_PATH + '2_2.yaml',
         'yaml'
     )
 
@@ -40,7 +42,7 @@ def copy_yaml_files(tmpdir_factory):
 def test_generate_diff_json(copy_json_files):
     """Test diff json files."""
     file_path1, file_path2 = copy_json_files
-    result_path = sys.path[0] + r'\fixtures\1_3.txt'
+    result_path = FIXTURE_PATH + '1_3.txt'
     with open(result_path, 'r') as opened_file:
         result_string = opened_file.read()
     assert generate_diff(file_path1, file_path2) == result_string
@@ -49,7 +51,7 @@ def test_generate_diff_json(copy_json_files):
 def test_generate_diff_yaml(copy_yaml_files):
     """Test diff yaml files."""
     file_path1, file_path2 = copy_yaml_files
-    result_path = sys.path[0] + r'\fixtures\1_3.txt'
+    result_path = FIXTURE_PATH + '1_3.txt'
     with open(result_path, 'r') as opened_file:
         result_string = opened_file.read()
     assert generate_diff(file_path1, file_path2) == result_string
