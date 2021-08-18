6dbf0edba37ca2fdf69f2621a2737944a8b1f063
ALezhnin <lezhnin.alex@gmail.com>
2021-08-18 08:10:31 +0300

    test7

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 """Script to start gendiff."""
 import argparse
+from pathlib import Path
 from gendiff import generate_diff
 from gendiff import stylish
 
@@ -11,10 +12,12 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
+    first_file = Path(args.first_file).resolve()
+    second_file = Path(args.second_file).resolve()
     if args.format:
-        print(generate_diff(args.first_file, args.second_file, args.format))
+        print(generate_diff(first_file, second_file, args.format))
     else:
-        print(generate_diff(args.first_file, args.second_file, stylish))
+        print(generate_diff(first_file, second_file, stylish))
 
 
 if __name__ == '__main__':
